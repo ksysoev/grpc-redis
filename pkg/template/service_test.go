0d95d35e97445715692701a07a88d5db23b25ef8
Kirill Sysoev <k.sysoev@me.com>
2024-07-10 22:23:23 +0800

    Adds test for rendering service template

---
+++ pkg/template/service_test.go
@@ -0,0 +1,54 @@
+package template
+
+import "testing"
+
+func TestRender(t *testing.T) {
+	s := &Service{
+		ServiceName:  "TestService",
+		FullName:     "com.example.TestService",
+		RedisClient:  "redisClient",
+		RPCServer:    "rpcServer",
+		NewRPCServer: "newRPCServer",
+		NewUUID:      "newUUID",
+		Methods:      []string{"Method1", "Method2"},
+	}
+
+	expected := `
+// TestService is the server API for com.example.TestService
+type RPCRedisTestService struct {
+    rpcSever *rpcServer
+	service  *TestServiceService
+}
+
+func NewRedisTestService(redis *redisClient, grpcService *TestServiceService) *RPCRedisTestService {
+	rpcServer := newRPCServer(redis, "com.example.TestService", "TestServiceGroup", newUUID().String())
+	service := &RPCRedisTestService{
+		rpcSever: rpcServer,
+		service:  grpcService,
+	}
+
+	// Register handlers
+	rpcServer.AddHandler("Method1", service.handleMethod1)
+	rpcServer.AddHandler("Method2", service.handleMethod2)
+
+	return service
+}
+
+func (x *RPCRedisTestService) Serve() error {
+	return x.rpcSever.Run()
+}
+
+func (x *RPCRedisTestService) Close() {
+	x.rpcSever.Close()
+}
+`
+
+	result, err := s.Render()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if result != expected {
+		t.Errorf("Rendered result does not match expected:\nExpected:\n%s\n\nGot:\n%s", expected, result)
+	}
+}
